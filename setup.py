b4c0ccd666a3f0c3a688dac3cf8ae28fe2c03db8
guochao <guochao@nutstore.net>
2018-04-03 15:32:46 +0800

    migrate to newer proxmox

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
-
 setup(name='proxmox-cli',
       version="1.0",
+      packages=["proxmoxcli"],
       description="control proxmox with cli",
-      install_requires=open("requirements.txt").read().splitlines()
+      url="https://github.com/jeffguorg/proxmox-cli",
+      scripts=["proxmoxcli/bin/proxmox"],
+      install_requires=open("requirements.txt").read().splitlines(),
      )
